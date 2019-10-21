b7b92e471045e4f0f7cf2b2809ce3a44f8c5634e
kous <kous@debiank.lan>
2019-10-21 20:47:22 +0200

    modif css base.html nav mobile ok

--- projet_ka/static/pages/js/base.js
+++ projet_ka/static/pages/js/base.js
@@ -163,12 +163,12 @@ $(function() {
 		console.log('menu_resize');
 		if(taille_p)
 		{
+			if($('div.bloc_principaux').hasClass("nav_open")){
+				$('#nav_bt_burger').trigger("click");
+			}
 			$('.bandeau_fond').hide();
 			$('.sousMenuMob').hide();
 			$('.subMenu1').css("display", "");  // Suppression de la propiété display: flex
-			if($('div.bloc_principaux').hasClass("nav_open")){
-				$('div.bloc_principaux').removeClass("nav_open");
-			}
 		}
 		else{
 			$('.bandeau_fond').slideDown();
