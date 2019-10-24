07f80e004fecd9b958507f5c461ca0dd2470c72a
kous <kous@debiank.lan>
2019-10-24 12:41:20 +0200

    login.html ok resetpwd.html en cours

--- projet_ka/users/forms.py
+++ projet_ka/users/forms.py
@@ -1,5 +1,8 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AuthenticationForm
+from django.utils.translation import gettext_lazy as _
 from .models import MyUser
+from django import forms
+from django.forms.widgets import TextInput
 
 
 class MyUserCreationForm(UserCreationForm):
@@ -12,6 +15,10 @@ class MyUserCreationForm(UserCreationForm):
 
 class MyUserChangeForm(UserChangeForm):
 
-    class Meta:
+    class Meta(UserChangeForm.Meta):
         model = MyUser
         fields = ('email',)
+
+class CustomAuthForm(AuthenticationForm):
+    username = forms.CharField(label=_("E-MAIL"), widget=forms.TextInput(attrs={'autofocus': True, 'class': 'inptext', 'type': 'email', 'placeholder':'Adresse e-mail'}))
+    password = forms.CharField(label=_("MOT DE PASSE"), strip=False, widget=forms.PasswordInput(attrs={'placeholder':'Mot de passe', 'class': 'inptext'}))
--- projet_ka/users/urls.py
+++ projet_ka/users/urls.py
@@ -18,12 +18,13 @@ from django.urls import path
 from django.conf.urls import url
 from users import views
 from django.contrib.auth import views as auth_views
+from .forms import CustomAuthForm
 
 
 urlpatterns = [
     #path('register', views.Registeruser, name='register'),
     path('register/', views.Registeruser.as_view(template_name='users/registeruser.html'), name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html', authentication_form=CustomAuthForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('password_change/', auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'), name='password_change_done'),
