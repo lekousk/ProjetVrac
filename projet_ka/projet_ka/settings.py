3dfe0f613ee2a89b682843ea92bef10839b5dc08
lekousk <45685249+lekousk@users.noreply.github.com>
2020-11-04 14:37:42 +0100

    Update settings.py

--- projet_ka/projet_ka/settings.py
+++ projet_ka/projet_ka/settings.py
@@ -115,7 +115,7 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'karim.elouadi@gmail.com'
+EMAIL_HOST_USER = 'xxx@gmail.com'
 EMAIL_HOST_PASSWORD = 'xxx'
 EMAIL_PORT = 587
 
